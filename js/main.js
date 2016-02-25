2ac843168ecbc450d6681d53034643a87c232f28
abautist <auggie.bautista@gmail.com>
2016-02-24 18:44:38 -0800

    conditional formatting

--- js/main.js
+++ js/main.js
@@ -11,9 +11,17 @@ var MyApp = React.createClass({
 	}
 });
 
+function formattedSeconds(sec) {
+	var seconds = ("0"+ sec % 60).slice(-2);
+	var minutes = Math.floor(sec/60);
+	return minutes + ":" + seconds;
+}
+
 var StopWatch = React.createClass({
 	getInitialState: function() {
-	    return { secondsElapsed: 0	          
+	    return { 
+	    	secondsElapsed: 0,
+	    	laps: []
 	    };
 	},
 	getSeconds: function(){
@@ -35,17 +43,31 @@ var StopWatch = React.createClass({
 		clearInterval(this.incrementer);
 		this.setState({ lastClearedIncrementer: this.incrementer})
 	},
+	handleLapClick: function() {
+		this.setState({ laps: this.state.laps.concat([this.state.secondsElapsed])});
+	},
 	handleResetClick: function() {
-		this.setState({ secondsElapsed: 0});
+		this.setState({ 
+			secondsElapsed: 0,
+			laps: []
+		});
 	},
 	render: function(){
 		return (
 			<div>
-				<h1>{this.getMinutes()}:{this.getSeconds()}</h1>
+				<h1>{formattedSeconds(this.state.secondsElapsed)}</h1>
 				{(this.state.secondsElapsed === 0 || this.incrementer === this.state.lastClearedIncrementer) ? 
 				 <button type="button" onClick={this.handleStartClick}>Start</button> :
 				 <button type="button" onClick={this.handleStopClick}>Stop</button> }
-				{this.state.secondsElapsed !== 0 ? <button type="button" onClick={this.handleResetClick}>Reset</button> : null} 
+				{(this.state.secondsElapsed !== 0 && this.incrementer !== this.state.lastClearedIncrementer) ? <button type="button" onClick={this.handleLapClick}>Lap</button> : null}
+				{(this.state.secondsElapsed !== 0 && this.incrementer === this.state.lastClearedIncrementer) ? <button type="button" onClick={this.handleResetClick}>Reset</button> : null}
+				<ul>
+					{this.state.laps.map(function(lap, idx){
+						return (
+							<li><strong>Lap {idx+1}</strong>/ {formattedSeconds(lap)}</li>
+						);
+					})}
+				</ul> 
 			</div>
 		);
 	}
